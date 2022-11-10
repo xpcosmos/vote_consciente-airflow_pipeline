083813b5613f2e3bcf7278366305ab38b9fbd038
xpcosmos <mikeias.d.s.o@gmail.com>
2022-11-10 14:06:17 -0300

    Criação de task mkdir_temp

--- airflow/dags/dados_politicos.py
+++ airflow/dags/dados_politicos.py
@@ -1,15 +1,22 @@
 from airflow import DAG
+from airflow.operators.bash import BashOperator
+import os
 
 
 from datetime import datetime
 
+path_temp_dir = os.path.abspath("./dados/temp")
 
 with DAG(
     dag_id = 'dados_politicos',
     description = 'Extração de dados políticos da câmara dos deputados',
     start_date= datetime.today()
 ) as dag:
-    pass
+    
+    task_1 = BashOperator(
+        task_id = 'mkdir_temp',
+        bash_command=  'mkdir {}'.format(path_temp_dir)
+    )
 
 
 
